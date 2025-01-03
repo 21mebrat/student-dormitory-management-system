9b10ef3952a1282b401dce7b469e317e43011dfb
Mebrat <129731218+21mebrat@users.noreply.github.com>
2025-01-03 20:11:43 +0300

    Update firebase.js

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -4,14 +4,6 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 import {getAuth} from 'firebase/auth'
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCNwhr1WbJjNIk5u8EosmTh31XDVflMkbE",
-  authDomain: "bookstore-9043d.firebaseapp.com",
-  projectId: "bookstore-9043d",
-  storageBucket: "bookstore-9043d.firebasestorage.app",
-  messagingSenderId: "446033843865",
-  appId: "1:446033843865:web:856d6b1871a696a0364a08"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
